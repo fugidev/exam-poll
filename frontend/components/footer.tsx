09ea19f74fccca69e51065692685073d86ebc0ff
Fugi <me@fugi.dev>
2023-09-02 01:17:16 +0200

    frontend: update footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,13 +1,15 @@
 import styles from 'styles/Footer.module.scss'
-import { FaGithub } from 'react-icons/fa'
+import { FaCodeBranch } from 'react-icons/fa'
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <span>
-        Developed by <a href="https://github.com/FugiMuffi" target="_blank" rel="noreferrer">Fugi</a>{' '}
-        and <a href="https://github.com/Trivo25" target="_blank" rel="noreferrer">Florian</a>{' '}
-        on <a href="https://github.com/FugiMuffi/exam-poll" target="_blank" rel="noreferrer" className={styles.github}>Github <FaGithub/></a>
+        Exam Poll is{' '}
+        <a href="https://github.com/FugiMuffi/exam-poll" target="_blank" rel="noreferrer" className={styles.sourceLink}>
+          Open Source
+          <FaCodeBranch/>
+        </a>
       </span>
     </footer>
   )
