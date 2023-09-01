a5856d74d391189125017595ef4c6f4393403d0b
Fugi <me@fugi.dev>
2023-09-02 01:00:23 +0200

    frontend: remove pie chart

--- frontend/components/voteResults.tsx
+++ frontend/components/voteResults.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import styles from 'styles/Poll.module.scss'
-import ResultPieChart from 'components/resultsPieChart'
 import ResultBarChart from 'components/resultsBarChart'
 
 type Props = {
@@ -10,10 +9,6 @@ type Props = {
 }
 
 const VoteResults: React.FC<Props> = ({ results }) => {
-  const [showBarChart, setShowBarChart] = useState(true)
-
-  const Chart = showBarChart ? ResultBarChart : ResultPieChart
-
   const voteCount = Object.values(results).reduce((a, b) => a + b, 0)
 
   const failureRate = Math.round(results['5.0'] / voteCount * 100)
@@ -24,17 +19,10 @@ const VoteResults: React.FC<Props> = ({ results }) => {
 
   return (
     <>
-      <Chart results={results} />
+      <ResultBarChart results={results} />
 
       <p>Total Votes: {voteCount}</p>
       <p>Failure Rate: {failureRate}%</p>
-
-      <button
-        className={styles.resultButton}
-        onClick={() => setShowBarChart(!showBarChart)}
-      >
-        Show {showBarChart ? 'Pie' : 'Bar'} Chart
-      </button>
     </>
   )
 }
