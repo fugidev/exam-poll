cb28c9d29cf580a033ae262e934075c5123d991f
Fugi <me@fugi.dev>
2024-01-09 19:38:21 +0100

    Add basic flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    (flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = {
+          # exam-poll-frontend = pkgs.callPackage ./frontend { };
+          # exam-poll-backend = pkgs.callPackage ./backend { };
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      })
+    ) // {
+      overlays.default = (_: prev: self.packages.${prev.system});
+
+      nixosModules.default = {
+        # imports = [ ./module.nix ];
+
+        nixpkgs.overlays = [ self.overlays.default ];
+      };
+    };
+}
