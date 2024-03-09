6d348b177fc0899cb317288cef8809ecec0fc830
Fugi <me@fugi.dev>
2024-03-09 23:47:39 +0100

    convert ResultsBarChart to function component

--- frontend/components/resultsBarChart.tsx
+++ frontend/components/resultsBarChart.tsx
@@ -1,5 +1,5 @@
 import * as d3 from 'd3'
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 
 type ResultBarChartProps = {
   results: {
@@ -8,24 +8,20 @@ type ResultBarChartProps = {
   initialFocus: boolean
 }
 
-class ResultBarChart extends Component<ResultBarChartProps> {
-  divRef: React.RefObject<HTMLDivElement>
-  data = Object.entries(this.props.results)
-  barWidth = 40
-  margin = 5
-  height = 360
-  width = (this.barWidth + this.margin) * this.data.length - this.margin
+const ResultBarChart: React.FC<ResultBarChartProps> = (props) => {
+  const data = Object.entries(props.results)
+  const barWidth = 40
+  const margin = 5
+  const height = 360
+  const width = (barWidth + margin) * data.length - margin
 
-  constructor(props: ResultBarChartProps) {
-    super(props)
-    this.divRef = React.createRef<HTMLDivElement>()
-  }
+  const [ divRef ] = useState(React.createRef<HTMLDivElement>())
 
-  componentDidMount() {
+  useEffect(() => {
     // initially focus the bar chart after pressing vote / show results, for screen readers
-    if (this.props.initialFocus) this.divRef.current.focus()
+    if (props.initialFocus) divRef.current.focus()
 
-    const totalCount = Object.values(this.props.results).reduce(
+    const totalCount = Object.values(props.results).reduce(
       (a, b) => a + b,
       0,
     )
@@ -37,41 +33,41 @@ class ResultBarChart extends Component<ResultBarChartProps> {
     }
 
     const svg = d3
-      .select(this.divRef.current)
+      .select(divRef.current)
       .append('svg')
-      .attr('viewBox', `0 0 ${this.width} ${this.height}`)
+      .attr('viewBox', `0 0 ${width} ${height}`)
       .attr('data-chart', 'bar')
 
     const scale = (d: any) =>
-      this.height * (d / Math.max(...this.data.map((v) => v[1])))
+      height * (d / Math.max(...data.map((v) => v[1])))
 
     const g = svg
       .selectAll('g')
-      .data(this.data.map((v) => v[1]))
+      .data(data.map((v) => v[1]))
       .enter()
       .append('g')
       .attr(
         'transform',
-        (d, i) => `translate(${i * (this.barWidth + this.margin)}, 0)`,
+        (d, i) => `translate(${i * (barWidth + margin)}, 0)`,
       )
       .attr('aria-label', (d, i) =>
-        getBarLabel(this.data[i][0], this.data[i][1]),
+        getBarLabel(data[i][0], data[i][1]),
       )
 
     g.append('rect')
       .attr('aria-hidden', true)
-      .attr('height', this.height)
-      .attr('width', this.barWidth)
+      .attr('height', height)
+      .attr('width', barWidth)
       .attr('fill', 'transparent')
 
     const g1 = g
       .append('g')
-      .attr('transform', (d) => `translate(0, ${this.height - scale(d)})`)
+      .attr('transform', (d) => `translate(0, ${height - scale(d)})`)
 
     g1.append('rect')
       .attr('aria-hidden', true)
       .attr('height', (d) => scale(d))
-      .attr('width', this.barWidth)
+      .attr('width', barWidth)
       .attr('fill', '#f7fff7')
 
     g1.append('text')
@@ -80,9 +76,9 @@ class ResultBarChart extends Component<ResultBarChartProps> {
       .attr('alignment-baseline', 'central')
       .attr('dy', '-1em')
       .attr('text-anchor', 'middle')
-      .attr('dx', this.barWidth / 2)
+      .attr('dx', barWidth / 2)
       .attr('transform', (d) => 'translate(' + 0 + ', ' + scale(d) + ')')
-      .text((d, i) => this.data[i][0])
+      .text((d, i) => data[i][0])
 
     g1.append('text')
       .attr('aria-hidden', true)
@@ -90,27 +86,25 @@ class ResultBarChart extends Component<ResultBarChartProps> {
       .attr('alignment-baseline', 'central')
       .attr('dy', '1em')
       .attr('text-anchor', 'middle')
-      .attr('dx', this.barWidth / 2)
-      .text((d, i) => this.data[i][1])
-  }
+      .attr('dx', barWidth / 2)
+      .text((d, i) => data[i][1])
+  }, [])
 
-  render() {
-    return (
-      <>
-        <style jsx>{`
-          div {
-            width: 100%;
-            max-width: ${this.width}px;
-            margin-bottom: 1rem;
-          }
-          div :global(svg text) {
-            mix-blend-mode: difference;
-          }
-        `}</style>
-        <div ref={this.divRef} tabIndex={-1}></div>
-      </>
-    )
-  }
+  return (
+    <>
+      <style jsx>{`
+        div {
+          width: 100%;
+          max-width: ${width}px;
+          margin-bottom: 1rem;
+        }
+        div :global(svg text) {
+          mix-blend-mode: difference;
+        }
+      `}</style>
+      <div ref={divRef} tabIndex={-1}></div>
+    </>
+  )
 }
 
 export default ResultBarChart
